f910472d62823c28c7739c97b7e190fcf657d3b0
Chadi El Masri <chadimsr@gmail.com>
2024-09-14 16:35:48 +0200

    remove trusted proxies

--- main.go
+++ main.go
@@ -29,7 +29,6 @@ var piscineMap = map[string]string{
 func main() {
 	r := gin.Default()
 	r.ForwardedByClientIP = true
-	r.SetTrustedProxies([]string{"127.0.0.1"})
 	r.GET("/api/piscine/:piscine", piscineHandler())
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
